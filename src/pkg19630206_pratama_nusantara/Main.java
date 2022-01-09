887daabb9f5f8668541ed782ad8cfa26b60f26a8
085219017321 <97389316+085219017321@users.noreply.github.com>
2022-01-10 05:31:08 +0800

    Add files via upload

---
+++ src/pkg19630206_pratama_nusantara/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg19630206_pratama_nusantara;
+
+/**
+ *
+ * @author IGEL
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Menu_Utama().setVisible(true);
+    }
+    
+}
